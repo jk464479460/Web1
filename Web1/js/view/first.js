7042d4191ded97fa9125ae1957c2f5c0a68db7c7
jk464479460 <464479460@qq.com>
2015-04-25 10:10:08 +0800

    yr

---
+++ Web1/js/view/first.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $(".index_up").click(function () {
+        $(".index_up").fadeOut("200", 
+        function () { $(".index_news").slideDown("2000"); });
+    });
+    $(".index_down").click(function () {
+        $(".index_news").slideUp("2000", 
+        function () { $(".index_up").fadeIn("200"); });
+       
+    });
+});
